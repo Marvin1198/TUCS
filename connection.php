5da7662b49dad8b19fac69e28b987e96c1bf4369
Marvin Patel <marvinpatel42@gmail.com>
2024-04-03 19:59:45 -0400

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$hostname = "db755317984.db.1and1.com";
-	$username = "dbo755317984";
-	$password = "MeshDroid@cake31";
-	$db_name = "db755317984";
-	$con = mysqli_connect($hostname,$username,$password,$db_name);
-?>
